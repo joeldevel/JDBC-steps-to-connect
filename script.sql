cc88d7b4e9651ae2a40c05ce86151dfea8222ce5
joeldevel <christianjoel.developer@gmail.com>
2022-04-24 15:55:03 -0300

    adds mysql script

---
+++ script.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE sample;
+
+CREATE TABLE fruits (name varchar(30), color varchar(30));
+
+INSERT INTO fruits (name, color) 
+			values 
+			('banana', 'yellow'), 
+			('apple', 'green'), 
+			('tangerine', 'orange');
